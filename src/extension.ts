d7955063728d92dcf2c71e9cb3c82c45fbeb4f35
krizzdewizz <gogo-900>
2015-11-23 23:16:37 +0100

    add first version

---
+++ src/extension.ts
@@ -0,0 +1,50 @@
+/**
+ * Provides the 'Extract variable' command.
+ */
+
+import * as vscode from 'vscode';
+
+function getIndent(text: string): string {
+    let s = '';
+    for (let i = 0, n = text.length; i < n; i++) {
+        const c = text[i];
+        if (c === ' ' || c === '\t') {
+            s += c;
+        } else {
+            break;
+        }
+    }
+    return s;
+}
+
+function extractVariable(): void {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor || editor.selection.isEmpty) {
+        return;
+    }
+
+    const doc = editor.document;
+    const sel = editor.selection;
+    const text = doc.getText(sel);
+    const lineText = doc.getText(new vscode.Range(new vscode.Position(sel.start.line, 0), new vscode.Position(sel.start.line, 30)));
+    const indent = getIndent(lineText);
+    const line = doc.lineAt(sel.start);
+    const theVar = 'xxx';
+    const prefix = `${indent}const `;
+    const allText = `${prefix}${theVar} = ${text};\n`;
+
+    editor.edit(builder => {
+        builder.insert(new vscode.Position(line.lineNumber, 0), allText);
+        builder.replace(sel, theVar);
+    }).then(ok => {
+        if (ok) {
+            editor.selections = [
+                new vscode.Selection(line.lineNumber, prefix.length, line.lineNumber, prefix.length + theVar.length),
+                new vscode.Selection(line.lineNumber + 1, sel.start.character, line.lineNumber + 1, sel.start.character + theVar.length)];
+        }
+    });
+}
+
+export function activate(context: vscode.ExtensionContext) {
+    context.subscriptions.push(vscode.commands.registerCommand('extension.refactorixExtractVariable', extractVariable));
+}
