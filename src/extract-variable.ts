578519a02463e71049dcdfc177508cf0618e9c43
Christian Oetterli <christian.oetterli@bison-group.com>
2016-05-01 16:15:07 +0200

    clean

--- src/extract-variable.ts
+++ src/extract-variable.ts
@@ -1,8 +1,8 @@
-import * as vscode from 'vscode';
+import * as vs from 'vscode';
 import {getIndentAtLine} from './refactor-vscode';
 
 export function extractVariable(): void {
-    const editor = vscode.window.activeTextEditor;
+    const editor = vs.window.activeTextEditor;
     if (!editor || editor.selection.isEmpty) {
         return;
     }
@@ -11,19 +11,19 @@ export function extractVariable(): void {
     const sel = editor.selection;
     const text = doc.getText(sel);
     const indent = getIndentAtLine(doc, sel.start.line);
-    const line = doc.lineAt(sel.start);
+    const line = doc.lineAt(sel.start).lineNumber;
     const theVar = 'xxx';
     const prefix = `${indent}const `;
     const allText = `${prefix}${theVar} = ${text};\n`;
 
     editor.edit(builder => {
-        builder.insert(new vscode.Position(line.lineNumber, 0), allText);
+        builder.insert(new vs.Position(line, 0), allText);
         builder.replace(sel, theVar);
     }).then(ok => {
         if (ok) {
             editor.selections = [
-                new vscode.Selection(line.lineNumber, prefix.length, line.lineNumber, prefix.length + theVar.length),
-                new vscode.Selection(line.lineNumber + 1, sel.start.character, line.lineNumber + 1, sel.start.character + theVar.length)];
+                new vs.Selection(line, prefix.length, line, prefix.length + theVar.length),
+                new vs.Selection(line + 1, sel.start.character, line + 1, sel.start.character + theVar.length)];
         }
     });
 }
