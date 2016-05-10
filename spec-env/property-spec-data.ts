c9874249e808c3d30e4d6217e2815cf3f3d0eb3c
Christian Oetterli <christian.oetterli@bison-group.com>
2016-05-11 00:26:04 +0200

    update

--- spec-env/property-spec-data.ts
+++ spec-env/property-spec-data.ts
@@ -1,11 +1,5 @@
 namespace property {
     class Color {
-        private _rgb: string;
-        get rgb(): string {
-            return this._rgb;
-        }
-        set rgb(value: string) {
-            this._rgb = value;
-        }
+        rgb: string;
     }
 }
